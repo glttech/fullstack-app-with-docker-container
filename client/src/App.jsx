7a230baea735ad74ee8294f7505af0981b31821d
Rahul Swami <50444725+glttech@users.noreply.github.com>
2025-11-05 20:13:10 +0530

    first push to production

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/message')
+      .then(response => response.json())
+      .then(data => {
+        setMessage(data.message)
+      .catch(error => {
+          console.error('Error fetching message:', error);
+        });
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to my frontend</h1>
+      <h2>Data</h2>
+      <p>{message}</p>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors(
+  {
+    origin: ['http://localhost:5173', 'http://localhost:5174',
+            'http://localhost:3000'],
+            // Add other allowed origins as needed like production frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+  }
+));
+// API Routes
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from Rahuls server using docker compose up!' });
+});
+const PORT = 4000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
